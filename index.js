13d2c1b0a38c71cc119527f6bf5c9e76893e1076
Jami <jamsheduddin03@gmail.com>
2023-03-03 10:01:35 +0600

    modal added

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-console.log("js file attached");
+// console.log("js file attached");
 
 const showCard = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => displayData(data.data.tools));
+    .then((data) => displaAllData(data.data.tools));
 };
 
-const displayData = (data) => {
+const displaAllData = (data) => {
   const container = document.getElementById("card-container");
-  console.log(container);
+  //   console.log(container);
   data.slice(0, 6).forEach((element) => {
     const innerContainer = document.createElement("div");
     innerContainer.innerHTML = `
       <div class="card card-compact p-3 bg-base-100 h-full shadow relative">
-                           <figure><img src="${element.image}" alt="Shoes" /></figure>
+                           <figure><img class='rounded-xl' src="${element.image}" alt="Shoes" /></figure>
            <div class="card-body mb-16">
                      <h2 class="card-title font-bold">Features</h2>
                      <ol class='list-decimal'>
@@ -23,14 +23,15 @@ const displayData = (data) => {
                      </ol>
                     
              <div class=' absolute bottom-0 right-0 left-0 p-3'>        
-                <div class="card-actions d-flex justify-between border-t py-2">
+                <div class="card-actions d-flex justify-between items-center border-t py-2">
                       <div>
                             <h1 class="text-xl font-bold">${element.name}</h1>
                              12/12/12
                       </div>
                       <div>
-                             <button class="rounded-full text-red-500  bg-red-100 px-4 py-3"><i
-                             class="fa-solid fa-arrow-right"></i></button>
+                      <label for="my-modal-3" onclick="cardDetail('${element.id}')" class="rounded-full text-red-500  bg-red-100 px-4 py-3 cursor-pointer"><i
+                      class="fa-solid fa-arrow-right"></i></label>
+                             
                       </div>
                 </div>
             </div>
@@ -43,4 +44,29 @@ const displayData = (data) => {
   });
 };
 
+const cardDetail = (id) => {
+  const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => showCardDetail(data));
+};
+
+const showCardDetail = () => {
+  const modalContainer = document.getElementById("modal-container");
+  const innerContainer = document.createElement("div");
+  innerContainer.innerHTML = `
+  <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+  <div class="modal">
+      <div class="modal-box w-11/12 max-w-5xl relative overflow-visible">
+          <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-[-16px] top-[-16px]">✕</label>
+          <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+          <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia
+              for free!</p>
+      </div>
+  </div>
+  
+  
+  `;
+  modalContainer.appendChild(innerContainer);
+};
 showCard();
